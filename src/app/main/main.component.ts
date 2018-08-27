f3143d5b792dca9800497897407c8f4aca8d2e18
Student <example@example.com>
2018-08-27 15:09:04 -0700

    album api call works

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,23 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { SpotifyAPIService } from '../spotify-api.service'
 import { Observable } from 'rxjs/Observable';
+import { Http, Response } from '@angular/http';
 
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
+  providers: [SpotifyAPIService]
 })
 export class MainComponent implements OnInit {
-  artist= "Bob Dylan"
+  artist = "Bob Dylan"
   albums: any[];
-  noPhotos: boolean=false;
 
-  constructor(private spotifyAPI: SpotifyAPIService)
-  {
-    this.spotifyAPI.login()
-      .subscribe(() => {
-        this.searchAlbums(this.artist);
+  constructor(private spotifyAPI: SpotifyAPIService) {}
+
+    ngOnInit() {
+      this.albums= [];
+      this.spotifyAPI.searchAlbums(this.artist).subscribe(response => {
+        console.log(response.albums.items)
+        if(response.albums.items.length > 0) {
+          this.albums = response;    
+        }
       });
     }
 
@@ -26,14 +31,7 @@ export class MainComponent implements OnInit {
         .subscribe(res => this.albums = res.albums.items)
     }
 
-  ngOnInit() {
-    this.albums=null;
-    this.spotifyAPI.searchAlbums(this.artist).subscribe(response => {
-      if(response.json().albums.length > 0) {
-        this.albums = response.json();
-      }
-    });
-  }
+
 }
 
 
