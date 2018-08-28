864522ddf4a983ccc851f0a3a4630225e11c34cb
ritabc <ritabc@gmail.com>
2018-08-28 16:29:37 -0700

    Update form, pass answers into .ts file

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { SpotifyAPIService } from '../spotify-api.service'
+import { SpotifyAPIService } from '../spotify-api.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -9,22 +10,28 @@ import { SpotifyAPIService } from '../spotify-api.service'
 })
 export class FormComponent implements OnInit {
 
-  constructor(private spotifyAPI: SpotifyAPIService) { 
-    
+  constructor(private spotifyAPI: SpotifyAPIService) {
+
   }
+  genre: string
   genres: any[];
 
   ngOnInit() {
+
     this.genres= [];
     this.spotifyAPI.loadGenres().subscribe(response => {
       console.log(response);
-      
+
       if(response.albums.items.length > 0) {
         this.genres = response.genres;
-         
+
       }
     });
   }
-  
+
+  tallyMoodAndSaveGenre(genre: string, q1Answer: string, q2Answer: string, q3Answer: string, q4Answer: string, q5Answer: string, q6Answer: string) {
+    this.genre = genre
+    console.log(this.genre)
+  }
 
 }
