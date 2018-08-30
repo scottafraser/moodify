caf2cb9f89840e4914a762b81cf9e2047006be00
Annie Shin <annieyshin@gmail.com>
2018-08-30 09:45:39 -0700

    css styling

--- src/app/spotify-api.service.ts
+++ src/app/spotify-api.service.ts
@@ -10,10 +10,7 @@ export class SpotifyAPIService {
   private client_id = "516dfc93cfe24870935c9d7112175615";
   private client_secret = "a7878c184adf45e181275d3d0052da69";
   private access_token: string;
-  private ArtistUrl: string;
-  private AlbumsUrl: string;
-  private GenreUrl: string;
-  private AlbumUrl: string;
+
   private encoded = btoa(this.client_id + ':' + this.client_secret);
   private base64 = 'OTk2MDgwOTM3ZWJiNDU5NGEwOTc5MTQ2YzljMGMxMjE6MGJkYTNjZmQyMTNjNDYyMmJjNmM1NjI1ODY1NjhlYzg=';
 
@@ -32,8 +29,6 @@ export class SpotifyAPIService {
 
 
   searchAlbums(str: string, token: string) {
-
-    console.log(this.encoded);
     this.searchUrl = 'https://api.spotify.com/v1/search?query=' + str + '&type=album';
     let headers = new Headers();
     headers.append('Authorization', 'Bearer ' + token);
@@ -43,13 +38,7 @@ export class SpotifyAPIService {
   }
 
   moodTracks(genre: string, valenceTarget: number, danceabilityTarget: number, tempoMin: number, tempoMax: number, token: string) {
-
-    console.log(this.encoded);
-    this.searchUrl = `https://api.spotify.com/v1/recommendations?market=US&seed_genres=${genre.toLowerCase()}&target_danceability=${danceabilityTarget}&limit=21&min_tempo=${tempoMin}&max_tempo=${tempoMax}&target_valence=${valenceTarget}`;
-
-    // this.searchUrl = `https://api.spotify.com/v1/recommendations?market=US&seed_genres=acoustic&target_danceability=0.5494912927359931&min_tempo=78.18716114414627&max_tempo=176.643113448108&target_valence=0.5641090053414818`
-    console.log(this.searchUrl);
-    
+    this.searchUrl = `https://api.spotify.com/v1/recommendations?market=US&seed_genres=${genre.toLowerCase()}&target_danceability=${danceabilityTarget}&limit=21&min_tempo=${tempoMin}&max_tempo=${tempoMax}&target_valence=${valenceTarget}`;    
     let headers = new Headers();
     headers.append('Authorization', 'Bearer ' + token);
 
@@ -58,52 +47,5 @@ export class SpotifyAPIService {
   }
 
 }
-
-// https:api.spotify.com/v1/recommendations?market=US&seed_genres=Acoustic&target_danceability=0.5424963387234651&min_tempo=81.26883129249934&max_tempo=182.70243837063552&target_valence=0.4948070699758
-// https:api.spotify.com/v1/recommendations?market=US&seed_genres=acoustic&target_danceability=0.5494912927359931&min_tempo=78.18716114414627&max_tempo=176.643113448108&target_valence=0.5641090053414818`
-
-
-
-  // loadGenres(token: string) {
-  //   this.GenreUrl = `https://api.spotify.com/v1/recommendations/available-genre-seeds`;
-  //   let headers = new Headers();
-  //   headers.append('Authorization', 'Bearer ' + token);
-
-  //   return this._http.get(this.GenreUrl, { headers: headers })
-  //     .map((res: Response) => res.json())
-  // }
-
-  // getArtist(id: string, token: string) {
-  //   this.ArtistUrl = 'https://api.spotify.com/v1/artists/' + id;
-  //   let headers = new Headers();
-  //   headers.append('Authorization', 'Bearer ' + token);
-
-  //   return this._http.get(this.ArtistUrl, { headers: headers })
-  //     .map((res: Response) => res.json())
-
-  // }
-
-
-  // getAlbums(artistId: string, token: string) {
-
-  //   this.AlbumsUrl = 'https://api.spotify.com/v1/artists/' + artistId + '/albums/?query=&limit=50';
-  //   let headers = new Headers();
-  //   headers.append('Authorization', 'Bearer ' + token);
-
-  //   return this._http.get(this.AlbumsUrl, { headers: headers })
-  //     .map((res: Response) => res.json())
-
-  // }
-
-  // getAlbum(id: string, token: string) {
-
-  //   this.AlbumUrl = 'https://api.spotify.com/v1/albums/' + id;
-  //   let headers = new Headers();
-  //   headers.append('Authorization', 'Bearer ' + token);
-
-  //   return this._http.get(this.AlbumUrl, { headers: headers })
-  //     .map((res: Response) => res.json())
-
-  // }
 
 
